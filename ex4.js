abf7cfdf60c9fad00b46f6d1168ad21e246827e8
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-01-23 10:14:41 +0900

    feat: ask함수 추가

--- ex4.js
+++ ex4.js
@@ -5,3 +5,10 @@ function ask(trigFn, resolveFn,rejectFn){
 const ask=(trigFn, resolveFn,rejectFn)=>{
     trigFn()?resolveFn():rejectFn()
 }
+
+ask(
+    ()=>confirm("동의하십니까?"),
+    ()=> alert("동의 완료"),
+    ()=> alert("동의하지않음")
+)
+
